7531508d00dd58f864acbe8c577a08e9f116bd0b
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-20 19:42:44 -0500

    Finished Random Math Equations.py
    I have finished this program that makes you take a quiz where the math questions are randomized. Im gonna use this to annoy my sister.

--- Random Math Equations.py
+++ Random Math Equations.py
@@ -1,4 +1,4 @@
-activate = -100000
+activate = 20
 add = 0
 sub = 0
 mul = 0
@@ -10,7 +10,7 @@ def math1():
         yield random.randint(1, 50)
 def math2():
     for i in range(1):
-        yield random.randint(3, 100)
+        yield random.randint(15, 100)
 def math3():
     for i in range(1):
         yield random.randint(1, 15)
@@ -57,4 +57,18 @@ while activate < 21:
                                     print("You are incorrect. The answer was %d" % (c))
                                     mul += 1
                             else: 
-                                if()
+                                if(div < 5):
+                                    c = a / d
+                                    ans = int(input("Answer the following division question (round to a whole number). %d / %d = " % (a, d)))
+                                    e = (round(c))
+                                    if(ans == e):
+                                        print("You are correct")
+                                        score += 1
+                                        div += 1
+                                    else:
+                                        print("You are incorrect. The answer was %d" % (round(c)))
+                                        div += 1
+                                else:
+                                    name = input("What is your name?: ")
+                                    print("Congratulations %s on finishing this math quiz. You got a %d/20 questions right. Thank you for taking this quiz and have a WONDERFUL day :)." % (name, score))
+                                    activate *= 100
