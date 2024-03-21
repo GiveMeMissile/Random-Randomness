48a72977ced399fbfa7e26226449e17efae5d33f
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-20 19:12:26 -0500

    Altered Random Math Equations.py
    No functionality changes. But a lot of changes to the code.

--- Random Math Equations.py
+++ Random Math Equations.py
@@ -1,4 +1,4 @@
-key = 0
+activate = -100000
 add = 0
 sub = 0
 mul = 0
@@ -7,53 +7,54 @@ score = 0
 import random
 def math1():
     for i in range(1):
-        yield random.randint(3, 50)
+        yield random.randint(1, 50)
 def math2():
     for i in range(1):
-        yield random.randint(3, 50)
-while key < 21:
+        yield random.randint(3, 100)
+def math3():
+    for i in range(1):
+        yield random.randint(1, 15)
+while activate < 21:
     for random_number in math2():
         a = random_number
         for random_number in math1():
             b = random_number
-            if(add < 6):
-                c = a + b
-                ans = int(input("Answer the following addition question. %d + %d = " % (a, b)))
-                if(ans == c):
-                    print("You are correct")
-                    key += 1
-                    add += 1
-                    score += 1
-                else:
-                    print("you are incorrect. The answer is %d" % (c))
-                    key += 1
-                    add += 1
-            else:
-                if(sub < 6):
-                    c = a - b
-                    ans = int(input("Answer the following subtraction question. %d - %d = " % (a, b)))
+            for random_number in math3():
+                d = random_number
+                if(add < 5):
+                    c = a + b
+                    ans = int(input("Answer the following addition question. %d + %d = " % (a, b)))
                     if(ans == c):
                         print("You are correct")
-                        key += 1
-                        sub += 1
+                        add += 1
                         score += 1
                     else:
-                        print("You are incorrect. The answer is %d" % (c))
-                        sub += 1
-                        key += 1
+                        print("you are incorrect. The answer is %d" % (c))
+                        add += 1
                 else:
-                    if(mul < 6):
-                        c = a * b
-                        ans = int(input("Answer the following multiplication question. %d * %d = " % (a, b)))
-                        if(ans == c):
-                            print("You are correct")
-                            key += 1
-                            score += 1
-                            mul += 1
-                        else:
-                            print("You are incorrect. The answer was %d" % (c))
-                        score += 1
+                    if(b > a) and (sub < 5):
+                        print("HI")
                     else:
-                        print("You are incorrect. The answer is %d" % (c))
-                        sub += 1
-                        key += 1
+                        if(sub < 5):
+                            c = a - b
+                            ans = int(input("Answer the following subtraction question. %d - %d = " % (a, b)))
+                            if(ans == c):
+                                print("You are correct")
+                                sub += 1
+                                score += 1
+                            else:
+                                print("You are incorrect. The answer is %d" % (c))
+                                sub += 1
+                        else:
+                            if(mul < 5):
+                                c = b * d
+                                ans = int(input("Answer the following multiplication question. %d * %d = " % (b, d)))
+                                if(ans == c):
+                                    print("You are correct")
+                                    score += 1
+                                    mul += 1
+                                else:
+                                    print("You are incorrect. The answer was %d" % (c))
+                                    mul += 1
+                            else: 
+                                if()
