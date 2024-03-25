b108d9c6430cd6d1d8e141c1125d0c13964179f3
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-03-25 10:18:25 -0500

    Buffed Random Math Equations.py
    Made multiplication harder. It was too easy for my little sister to do. So I made it harder but not too are for her.

--- Random Math Equations.py
+++ Random Math Equations.py
@@ -14,6 +14,9 @@ def math2():
 def math3():
     for i in range(1):
         yield random.randint(1, 15)
+def math4():
+    for i in range(1):
+        yield random.randint(6, 25)
 while activate < 21:
     for random_number in math2():
         a = random_number
@@ -21,54 +24,56 @@ while activate < 21:
             b = random_number
             for random_number in math3():
                 d = random_number
-                if(add < 5):
-                    c = a + b
-                    ans = int(input("Answer the following addition question. %d + %d = " % (a, b)))
-                    if(ans == c):
-                        print("You are correct")
-                        add += 1
-                        score += 1
-                    else:
-                        print("you are incorrect. The answer is %d" % (c))
-                        add += 1
-                else:
-                    if(b > a) and (sub < 5):
-                        print("HI")
-                    else:
-                        if(sub < 5):
-                            c = a - b
-                            ans = int(input("Answer the following subtraction question. %d - %d = " % (a, b)))
-                            if(ans == c):
-                                print("You are correct")
-                                sub += 1
-                                score += 1
-                            else:
-                                print("You are incorrect. The answer is %d" % (c))
-                                sub += 1
+                for random_number in math4():
+                    e = random_number
+                    if(add < 5):
+                        c = a + b
+                        ans = int(input("Answer the following addition question. %d + %d = " % (a, b)))
+                        if(ans == c):
+                            print("You are correct")
+                            add += 1
+                            score += 1
                         else:
-                            if(mul < 5):
-                                c = b * d
-                                ans = int(input("Answer the following multiplication question. %d * %d = " % (b, d)))
+                            print("you are incorrect. The answer is %d" % (c))
+                            add += 1
+                    else:
+                        if(b > a) and (sub < 5):
+                            print("HI")
+                        else:
+                            if(sub < 5):
+                                c = a - b
+                                ans = int(input("Answer the following subtraction question. %d - %d = " % (a, b)))
                                 if(ans == c):
                                     print("You are correct")
+                                    sub += 1
                                     score += 1
-                                    mul += 1
                                 else:
-                                    print("You are incorrect. The answer was %d" % (c))
-                                    mul += 1
-                            else: 
-                                if(div < 5):
-                                    c = a / d
-                                    ans = int(input("Answer the following division question (round to a whole number). %d / %d = " % (a, d)))
-                                    e = (round(c))
-                                    if(ans == e):
+                                    print("You are incorrect. The answer is %d" % (c))
+                                    sub += 1
+                            else:
+                                if(mul < 5):
+                                    c = b * e
+                                    ans = int(input("Answer the following multiplication question. %d * %d = " % (b, e)))
+                                    if(ans == c):
                                         print("You are correct")
                                         score += 1
-                                        div += 1
+                                        mul += 1
                                     else:
-                                        print("You are incorrect. The answer was %d" % (round(c)))
-                                        div += 1
-                                else:
-                                    name = input("What is your name?: ")
-                                    print("Congratulations %s on finishing this math quiz. You got a %d/20 questions right. Thank you for taking this quiz and have a WONDERFUL day :)." % (name, score))
-                                    activate *= 100
+                                        print("You are incorrect. The answer was %d" % (c))
+                                        mul += 1
+                                else: 
+                                    if(div < 5):
+                                        c = a / d
+                                        ans = int(input("Answer the following division question (round to a whole number). %d / %d = " %  (a, d)))
+                                        e = (round(c))
+                                        if(ans == e):
+                                            print("You are correct")
+                                            score += 1
+                                            div += 1
+                                        else:
+                                            print("You are incorrect. The answer was %d" % (round(c)))
+                                            div += 1
+                                    else:
+                                        name = input("What is your name?: ")
+                                        print("Congratulations %s on finishing this math quiz. You got a %d/20 questions right. Thank you for taking this quiz and have a WONDERFUL day :)." % (name, score))
+                                        activate *= 100
