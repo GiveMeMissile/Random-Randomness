68f39af2857975e49f7687892f70f2d0386ccfd0
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-04-05 15:42:10 -0500

    Updated Random Algebra.py
    Quick Fix

--- Random Algebra.py
+++ Random Algebra.py
@@ -1,22 +1,22 @@
 import random
 key = 0
 name = input("What is your name?: ")
-def rand():
-    yield random.randint(1, 7)
-def ran():
-    yield random.randint(2, 6)
-def rann():
-    yield random.randint(1, 12)
-def randd():
-    yield random.randint(9, 50)
+print("Hello %s. Welcome to the randomly generated algebra test. You will be aksed a series of randomly generated algebra questions. There will be 15 questions in total. Good luck." % name)
 def Math(x, a, b, e):
     if(key < 5):
         c = b*x - e
+        return c
+    elif(key < 10):
+        t = b*x + e
+        c = t-a*x
         return c
 def question(x, a, b, e):
     c = Math(x, a, b, e)
     if(key < 5):
         ans = int(input("Solve for x, %dx - %d = %d?: " % (b, e, c)))
+        return ans
+    elif(key < 10):
+        ans = int(input("Solve for x, %dx + %d = %dx + %d?: " % (b, e, a, c)))
         return ans
 def results(x, a, b, e):
     ans = question(x, a, b, e)
