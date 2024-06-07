f805b75b043a016ded4b0088474757d328a0605e
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-06-06 21:39:47 -0500

    Random Quadratics.py was born

---
+++ Random Quadratics.py
@@ -0,0 +1,11 @@
+import time
+import pandas as PANDAAAAAAAA
+import random
+def rand():
+  a = random.randint(1,15)
+  b = random.randint(1,15)
+def quad(question):
+  if question < 6:
+    
+
+question = 0
