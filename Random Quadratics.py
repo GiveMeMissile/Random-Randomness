648c7607c640a8b910609a53213598dbb0005238
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-06-08 16:13:46 -0500

    New stuff Random Quadratics.py
    MATH IS ALL POWERFUL. MATH IS ALL. ACCEPT MATH AS YOUR NEW LORD AND SAVIOR.

--- Random Quadratics.py
+++ Random Quadratics.py
@@ -4,8 +4,15 @@ import random
 def rand():
   a = random.randint(1,15)
   b = random.randint(1,15)
-def quad(question):
+  return a, b
+def quad(question, a, b):
+  quest = ("(x + %d)(x + %d)" % (a, b))
+  c = a * b
+  d = a + b
+  answer = ("x^2 + %dx + %d" % (d, c))
   if question < 6:
-    
-
+    return quest, answer
+  elif question < 11:
+    return answer, quest
 question = 0
+a, b = rand()
