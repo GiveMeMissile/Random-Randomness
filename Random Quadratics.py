1056d98bc388e578a44ba09c961cf96be3da5c93
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-06-08 17:43:27 -0500

    Update Random Quadratics.py

--- Random Quadratics.py
+++ Random Quadratics.py
@@ -20,8 +20,24 @@ def quad(question, a, b, e):
     r = a + (e * b)
     answer = ("%dx^2 + %dx + %d" % (e, r, c))
     return quest, answer
-def quiz(quest, answer):
-  
-
+def quiz(quest, answer, question):
+  print("You must answer the quadratic equation like this, Ex: x^2 + 2x + 4, or Ex: (x+1)(x+2)")
+  for quests, answers in zip(quest, answer):
+    question += 1
+    start = time.time
+    ans = input("Solve the following problem. %s: " % (quest))
+    end = time.time
+    total = end - start
+    if ans == answer:
+      print("Correct the answer was %s" % (answer))
+      corint = "correct"
+    else:
+      print("Incorrect the answer was %s" % (answer))
+      corint = "incorrect"
+    dict = {"Question": [quests], "Answer": [answers], "Your answer": [ans], "Correct or incorrect": [corint], "Time taken": [total]}
+    return results
 question = 0
 a, b, e = rand()
+quest, answer = quad(question, a, b, e)
+results = quiz(quest, answer, question)
+print(results)
