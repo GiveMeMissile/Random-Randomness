65cd9305f990ef55fb69cbbdf0cec9a3e767b798
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-11-14 11:47:53 -0600

    Update Random_math_C#.cs
    Added some things. Still does not work.

--- Random_math_C#.cs
+++ Random_math_C#.cs
@@ -8,15 +8,35 @@ public class HelloWorld
         int thing = random.Next(0, 20);
         Console.WriteLine(thing);
         int correct = 0;
-        public int Addition(correct)
+        int totalQuestions = 0;
+        while(true)
         {
-            while(true)
+            int a = random.Next(15,125);
+            int b = random.Next(15,125);
+            int c = a+b;
+            Console.WriteLine("What is "+a+"+"+b+"?");
+            try
             {
-                int a = random.Next(15,125);
-                int b = random.Next(15,125);
-                int c = a+b;
-                Console.WriteLine()
+                int answer = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Don't input a letter or decimal.");
+                answer = -1;
+            }
+            if(answer == c)
+            {
+                Console.WriteLine("You are correct. The answer is "+c);
+                correct++;
+            }
+            else
+            {
+                Console.WriteLine("You are Incorrect. The answer is "+c);
+            }
+            totalQuestions++;
+            if(totalQuestions >= 5)
+            {
+                break;
             }
         }
     }
-}
