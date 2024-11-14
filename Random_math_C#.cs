6b950f0cfde157111f7e65a5408300c22c87e066
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-11-14 11:34:20 -0600

    Random_math_C#.cs was born
    I am beginning to learn C#!

---
+++ Random_math_C#.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+        int thing = random.Next(0, 20);
+        Console.WriteLine(thing);
+        int correct = 0;
+        public int Addition(correct)
+        {
+            while(true)
+            {
+                int a = random.Next(15,125);
+                int b = random.Next(15,125);
+                int c = a+b;
+                Console.WriteLine()
+            }
+        }
+    }
+}
