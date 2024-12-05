139c3a7b1029bb404d1460443e7b8d7179687ef9
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-12-04 18:38:49 -0600

    Update Random_math_C#.cs
    I am now using methods (:

--- Random_math_C#.cs
+++ Random_math_C#.cs
@@ -5,11 +5,12 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         int correct = 0;
+        int totalQuestions = 0;
         Console.WriteLine("Hello. You will be taking a 20 question quiz. Where you will need to answer 20 very easy math questions. Good luck (:");
-        correct = Addition(correct);
+        correct = Addition(correct, totalQuestions);
         
     }
-    static int Addition(int correct)
+    static int Addition(int correct, int totalQuestions)
     {
         while (true)
         {
@@ -17,7 +18,20 @@ public class HelloWorld
             int a = random.Next(15,150);
             int b = random.Next(15,150);
             int c = a+b;
-            break;
+            int answer = -1;
+            Console.WriteLine("What is "+a+ "+" +b+"?:");
+            try
+            {
+                answer = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You did not input an int, Please try aagin");
+            }
+            if(answer == c)
+            {
+                
+            }
         }
         return correct;
     }
