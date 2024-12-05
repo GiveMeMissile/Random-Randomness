1130b50ad3ba8ae92e2141ebd8850a8fbf7ebb07
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-12-05 13:22:42 -0600

    Update Random_math_C#.cs
    Finished addition.

--- Random_math_C#.cs
+++ Random_math_C#.cs
@@ -1,17 +1,17 @@
 using System;
 
+
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
         int correct = 0;
-        int totalQuestions = 0;
         Console.WriteLine("Hello. You will be taking a 20 question quiz. Where you will need to answer 20 very easy math questions. Good luck (:");
-        correct = Addition(correct, totalQuestions);
-        
+        correct = Addition(correct);
     }
-    static int Addition(int correct, int totalQuestions)
+    static int Addition(int correct)
     {
+        int questions = 0;
         while (true)
         {
             Random random = new Random();
@@ -23,16 +23,38 @@ public class HelloWorld
             try
             {
                 answer = Convert.ToInt32(Console.ReadLine());
+                if(answer == -1)
+                {
+                    answer++;
+                }
             }
             catch (Exception e)
             {
-                Console.WriteLine("You did not input an int, Please try aagin");
+                Console.WriteLine("You did not input an int");
             }
             if(answer == c)
             {
-                
+                Console.WriteLine("You are correct. The answer is "+c+".");
+                correct++;
+                questions++;
+            }
+            else if(answer == -1)
+            {}
+            else
+            {
+                Console.WriteLine("You are incorrect. The answer is "+c+".");
+                questions++;
+            }
+            if(questions >= 5)
+            {
+                Console.WriteLine("That is the end of the addition section.");
+                break;
             }
         }
         return correct;
+    }
+    static int Subtraction(correct)
+    {
+        
     }
 }
